c5b0a7f46469fa68c6004404c5d928ca65cfb3fc
Satish Babariya <satish.babariya@gmail.com>
2019-06-02 09:26:14 +0000

    initial commit

--- Sources/Resty/Resty.swift
+++ Sources/Resty/Resty.swift
@@ -1,3 +1,87 @@
-struct Resty {
-    var text = "Hello, World!"
+import Foundation
+
+/// The protocol used to define the specifications necessary for a `Restly`.
+public protocol Restly {
+    /// The host, conforming to RFC 1808.
+    var host: String { get }
+
+    /// The path, conforming to RFC 1808
+    var path: String { get }
+
+    /// API Endpoint
+    var endpoint: String { get }
+
+    /// The HTTP method used in the request.
+    var method: HTTPMethod { get }
+
+    /// The HTTP request parameters.
+    var parameters: [String: Any]? { get }
+
+    /// A dictionary containing all the HTTP header fields
+    var headers: [String: String]? { get }
+}
+
+/// HTTP Methods
+public enum HTTPMethod: String {
+    case get = "GET"
+    case head = "HEAD"
+    case post = "POST"
+    case put = "PUT"
+    case patch = "PATCH"
+    case delete = "DELETE"
+    case trace = "TRACE"
+    case connect = "CONNECT"
+    case options = "OPTIONS"
+}
+
+/// Restly Errors
+enum RestlyError: Error {
+    case badURL
+}
+
+extension Restly {
+    /// The URL of the receiver.
+    fileprivate var url: String {
+        return host + path
+    }
+
+    func request<T: Codable>(type: T.Type, completionHandler: @escaping (Result<T, Error>) -> Void) {
+        guard let url = URL(string: url) else {
+            completionHandler(.failure(RestlyError.badURL))
+            return
+        }
+
+        var request = URLRequest(url: url)
+        request.httpMethod = method.rawValue
+        request.allHTTPHeaderFields = headers
+        request.cachePolicy = .reloadIgnoringLocalAndRemoteCacheData
+
+        if let parameters = parameters {
+            do {
+                request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: [])
+            } catch {
+                completionHandler(.failure(error))
+                return
+            }
+        }
+
+        let session = URLSession.shared
+        let dataTask = session.dataTask(with: request, completionHandler: { (data, _, error) -> Void in
+            if let error = error {
+                completionHandler(.failure(error))
+                return
+            }
+
+            if let data = data {
+                do {
+                    completionHandler(.success(try JSONDecoder().decode(type, from: data)))
+                } catch {
+                    completionHandler(.failure(error))
+                    return
+                }
+            }
+        })
+
+        dataTask.resume()
+    }
 }
